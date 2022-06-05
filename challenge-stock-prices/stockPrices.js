f4d9c2265683b570235047c5be6f9396ca159c4c
Logan Leatherow <lleatherow@niagarawater.com>
2022-06-05 10:39:09 -0500

    stock prices done

---
+++ challenge-stock-prices/stockPrices.js
@@ -0,0 +1,20 @@
+const best = prices => {
+    let maxDiff = 0
+    let lowSoFar
+  
+    for (let i = 0; i < prices.length; i++) {
+      if (lowSoFar === undefined || prices[i] < lowSoFar) {
+        lowSoFar = prices[i]
+      }
+  
+      let diff = prices[i] - lowSoFar
+  
+      if (diff > maxDiff) {
+        maxDiff = diff
+      }
+    }
+  
+    return maxDiff
+  }
+let prices = [10,20,30,25,21,14,16,25]
+  console.log ( best(prices))
